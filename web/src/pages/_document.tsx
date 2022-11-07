f24fff95ff14c06fbf4914459f96a808038cafcf
Izaias Lima <53953937+IzaiasMorais@users.noreply.github.com>
2022-11-07 11:16:50 -0300

    fix(web): env variable visibility

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -5,11 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap"
           rel="stylesheet"
