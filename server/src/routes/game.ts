8abb7284514f819832f1974dc80dd44f45b3ed49
Izaias Lima <53953937+IzaiasMorais@users.noreply.github.com>
2022-11-10 10:49:54 -0300

    chore(server): finished almost all backend routes

--- server/src/routes/game.ts
+++ server/src/routes/game.ts
@@ -1,4 +1,44 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { authenticate } from "../plugins/authenticate";
 
-export async function gameRoutes(fastify: FastifyInstance) {}
+export async function gameRoutes(fastify: FastifyInstance) {
+  fastify.get(
+    "/polls/:id/games",
+    { onRequest: [authenticate] },
+    async (req) => {
+      const getPollParams = z.object({
+        id: z.string(),
+      });
+
+      const { id } = getPollParams.parse(req.params);
+
+      const games = await prisma.game.findMany({
+        orderBy: {
+          date: "desc",
+        },
+        include: {
+          guesses: {
+            where: {
+              participant: {
+                userId: req.user.sub,
+                pollId: id,
+              },
+            },
+          },
+        },
+      });
+
+      return {
+        games: games.map((game) => {
+          return {
+            ...game,
+            guess: game.guesses.length > 0 ? game.guesses[0] : null,
+            guesses: undefined,
+          };
+        }),
+      };
+    }
+  );
+}
--- server/src/routes/guess.ts
+++ server/src/routes/guess.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { authenticate } from "../plugins/authenticate";
 
 export async function guessRoutes(fastify: FastifyInstance) {
   fastify.get("/guesses/count", async () => {
@@ -7,4 +9,89 @@ export async function guessRoutes(fastify: FastifyInstance) {
 
     return { count };
   });
+
+  fastify.post(
+    "/polls/:pollId/games/:gameId/guesses",
+    { onRequest: [authenticate] },
+    async (request, reply) => {
+      const createGuessParams = z.object({
+        pollId: z.string(),
+        gameId: z.string(),
+      });
+
+      const createGuessBody = z.object({
+        firstTeamPoints: z.number(),
+        secondTeamPoints: z.number(),
+      });
+
+      // Validate if the variables are compatile with the required types
+      const { firstTeamPoints, secondTeamPoints } = createGuessBody.parse(
+        request.body
+      );
+      // Validate if the variables are compatile with the required types
+      const { pollId, gameId } = createGuessParams.parse(request.params);
+
+      // Verify if the participant are registered in the poll
+      const participant = await prisma.participant.findUnique({
+        where: {
+          userId_pollId: {
+            pollId,
+            userId: request.user.sub,
+          },
+        },
+      });
+
+      if (!participant) {
+        return reply.status(400).send({
+          message: "You are not allowed to create a guess inside this poll",
+        });
+      }
+
+      // Verify if the participant already made a guess in the poll
+      const guess = await prisma.guess.findUnique({
+        where: {
+          gameId_participantId: {
+            gameId,
+            participantId: participant.id,
+          },
+        },
+      });
+
+      if (guess) {
+        return reply.status(400).send({
+          message: "You already made a guess on this poll",
+        });
+      }
+
+      // Find the game
+      const game = await prisma.game.findUnique({
+        where: {
+          id: gameId,
+        },
+      });
+
+      if (!game) {
+        return reply.status(404).send({
+          message: "Game does not exist",
+        });
+      }
+
+      if (game.date < new Date()) {
+        return reply.status(400).send({
+          message: "You cannot send guesses after the game date",
+        });
+      }
+
+      await prisma.guess.create({
+        data: {
+          gameId,
+          participantId: participant.id,
+          firstTeamPoints,
+          secondTeamPoints,
+        },
+      });
+
+      return reply.status(201).send();
+    }
+  );
 }
