bb424933740ff71a9390c89a65ade279c0ed016d
Izaias Lima <53953937+IzaiasMorais@users.noreply.github.com>
2022-11-14 11:41:02 -0300

    chore(server): implemented create game route

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,61 +1,61 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: "John Doe",
-      email: "john.doe@gmail.com",
-      avatarUrl: "https://github.com/izaiasmorais.png",
-    },
-  });
+// async function main() {
+//   const user = await prisma.user.create({
+//     data: {
+//       name: "John Doe",
+//       email: "john.doe@gmail.com",
+//       avatarUrl: "https://github.com/izaiasmorais.png",
+//     },
+//   });
 
-  const poll = await prisma.poll.create({
-    data: {
-      title: "Example poll",
-      code: "BOL123",
-      ownerId: user.id,
+//   const poll = await prisma.poll.create({
+//     data: {
+//       title: "Example poll",
+//       code: "BOL123",
+//       ownerId: user.id,
 
-      participants: {
-        create: {
-          userId: user.id,
-        },
-      },
-    },
-  });
+//       participants: {
+//         create: {
+//           userId: user.id,
+//         },
+//       },
+//     },
+//   });
 
-  await prisma.game.create({
-    data: {
-      date: "2022-11-18T12:00:00.201Z",
-      firstTeamCountryCode: "DE",
-      secondTeamCountryCode: "BR",
-    },
-  });
+//   await prisma.game.create({
+//     data: {
+//       date: "2022-11-18T12:00:00.201Z",
+//       firstTeamCountryCode: "DE",
+//       secondTeamCountryCode: "BR",
+//     },
+//   });
 
-  await prisma.game.create({
-    data: {
-      date: "2022-11-19T12:00:00.201Z",
-      firstTeamCountryCode: "BR",
-      secondTeamCountryCode: "AR",
+//   await prisma.game.create({
+//     data: {
+//       date: "2022-11-19T12:00:00.201Z",
+//       firstTeamCountryCode: "BR",
+//       secondTeamCountryCode: "AR",
 
-      guesses: {
-        create: {
-          firstTeamPoints: 2,
-          secondTeamPoints: 1,
+//       guesses: {
+//         create: {
+//           firstTeamPoints: 2,
+//           secondTeamPoints: 1,
 
-          participant: {
-            connect: {
-              userId_pollId: {
-                userId: user.id,
-                pollId: poll.id,
-              },
-            },
-          },
-        },
-      },
-    },
-  });
-}
+//           participant: {
+//             connect: {
+//               userId_pollId: {
+//                 userId: user.id,
+//                 pollId: poll.id,
+//               },
+//             },
+//           },
+//         },
+//       },
+//     },
+//   });
+// }
 
-main();
+// main();
--- server/src/routes/game.ts
+++ server/src/routes/game.ts
@@ -41,4 +41,53 @@ export async function gameRoutes(fastify: FastifyInstance) {
       };
     }
   );
+
+  fastify.post(
+    "/polls/:id/games",
+    { onRequest: [authenticate] },
+    async (request, reply) => {
+      const getPollParams = z.object({
+        id: z.string(),
+      });
+
+      const { id } = getPollParams.parse(request.params);
+
+      const poll = await prisma.poll.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!poll) {
+        return reply.status(404).send({
+          message: "Poll not found",
+        });
+      }
+
+      if (poll.ownerId !== request.user.sub) {
+        return reply.status(400).send({
+          message: "You are not allowed to create a game inside this poll",
+        });
+      }
+
+      const createGameBody = z.object({
+        date: z.string(),
+        firstTeamCountryCode: z.string(),
+        secondTeamCountryCode: z.string(),
+      });
+
+      const { date, firstTeamCountryCode, secondTeamCountryCode } =
+        createGameBody.parse(request.body);
+
+      await prisma.game.create({
+        data: {
+          date: new Date(date),
+          firstTeamCountryCode,
+          secondTeamCountryCode,
+        },
+      });
+
+      return reply.status(201).send();
+    }
+  );
 }
